382896db5ea99270d0a18c05525c2e0bc80807c8
Jeremy Myers <echelonical@gmail.com>
2023-04-04 15:43:00 -0500

    created a method

--- Program.cs
+++ Program.cs
@@ -43,88 +43,115 @@ string greeting = @"Welcome to Thrown For a Loop
 Your one-stop shop for used sporting equipment";
 
 Console.WriteLine(greeting);
-Console.WriteLine("Products:");
 
-decimal totalValue = 0.0M;
-foreach (Product product in products)
+//? initial call of method built below:
+// ViewProductDetails();
+
+string choice = null;
+while (choice != "0")
 {
-    if (!product.Sold)
+    Console.WriteLine(@"Choose an option:
+                        0. Exit
+                        1. View Product Details");
+    choice = Console.ReadLine();
+    if (choice == "0")
     {
-        totalValue += product.Price;
+        Console.WriteLine("Goodbye!");
+    }
+    else if (choice == "1")
+    {
+        ViewProductDetails();
     }
 }
-Console.WriteLine($"Total inventory value: ${totalValue}");
 
-for (int i = 0; i < products.Count; i++)
+
+
+
+void ViewProductDetails()
 {
-    Console.WriteLine($"{i + 1}. {products[i]?.Name}");
-}
+    Console.WriteLine("Products:");
 
-//? Updated exception handling code for product (FormatException or ArgumentOutOfRangeException)
-Product chosenProduct = null;
-// while chosenProduct (which is the response from user) is null
-while (chosenProduct == null)
-{
-    // run this code block
-    Console.WriteLine("Please enter a product number: ");
+    decimal totalValue = 0.0M;
+    foreach (Product product in products)
+    {
+        if (!product.Sold)
+        {
+            totalValue += product.Price;
+        }
+    }
+    Console.WriteLine($"Total inventory value: ${totalValue}");
 
+    for (int i = 0; i < products.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {products[i]?.Name}");
+    }
+
+    //? Updated exception handling code for product (FormatException or ArgumentOutOfRangeException)
+    Product chosenProduct = null;
+    // while chosenProduct (which is the response from user) is null
+    while (chosenProduct == null)
+    {
+        // run this code block
+        Console.WriteLine("Please enter a product number: ");
+
+        try
+        {
+            int response = int.Parse(Console.ReadLine().Trim());
+            chosenProduct = products[response - 1];
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Please type only integers!");
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Please choose an existing item only!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            Console.WriteLine("Do Better!");
+        }
+    }
+
+    //* initial try/catch for exceptions with updates as course progressed.
+    /*
     try
     {
         int response = int.Parse(Console.ReadLine().Trim());
         chosenProduct = products[response - 1];
     }
-    catch (FormatException)
-    {
-        Console.WriteLine("Please type only integers!");
-    }
-    catch (ArgumentOutOfRangeException)
-    {
-        Console.WriteLine("Please choose an existing item only!");
-    }
-    catch (Exception ex)
+    // if an exception occurs respond with the below message and reenter chosenProduct null phase so user can enter correct format.
+    // this catches the exception in ex variable!!!
+    catch (Exception ex) //! Didn't have to specify the exception to catch?? 
     {
         Console.WriteLine(ex);
-        Console.WriteLine("Do Better!");
+        Console.WriteLine("Do better!");
     }
-}
-
-//* initial try/catch for exceptions with updates as course progressed.
-/*
-try
-{
-    int response = int.Parse(Console.ReadLine().Trim());
-    chosenProduct = products[response - 1];
-}
-// if an exception occurs respond with the below message and reenter chosenProduct null phase so user can enter correct format.
-// this catches the exception in ex variable!!!
-catch (Exception ex) //! Didn't have to specify the exception to catch?? 
-{
-    Console.WriteLine(ex);
-    Console.WriteLine("Do better!");
-}
-}
-*/
-
-//* Old product prompt without exception handling (FormatException or ArgumentOutOfRangeException)
-Console.WriteLine("Please enter a product number: ");
-/*
-    int response = int.Parse(Console.ReadLine().Trim()); //.Trim() returns a string
-    while (response > products.Count || response < 1)
-    // while (string.IsNullOrEmpty(response))
-    {
-        Console.WriteLine(@"Choose a number between 1 and 4!");
-        response = int.Parse(Console.ReadLine().Trim());
     }
+    */
 
-    Product chosenProduct = products[response - 1];
-*/
+    //* Old product prompt without exception handling (FormatException or ArgumentOutOfRangeException)
+    Console.WriteLine("Please enter a product number: ");
+    /*
+        int response = int.Parse(Console.ReadLine().Trim()); //.Trim() returns a string
+        while (response > products.Count || response < 1)
+        // while (string.IsNullOrEmpty(response))
+        {
+            Console.WriteLine(@"Choose a number between 1 and 4!");
+            response = int.Parse(Console.ReadLine().Trim());
+        }
 
-DateTime now = DateTime.Now;
-TimeSpan timeInStock = now - chosenProduct.StockDate;
+        Product chosenProduct = products[response - 1];
+    */
 
-Console.WriteLine(@$"You chose: {chosenProduct.Name}, which costs {chosenProduct.Price} dollars.
+    DateTime now = DateTime.Now;
+    TimeSpan timeInStock = now - chosenProduct.StockDate;
+
+    Console.WriteLine(@$"You chose: {chosenProduct.Name}, which costs {chosenProduct.Price} dollars.
 It is {now.Year - chosenProduct.ManufactureYear} years old. The condition is graded {chosenProduct.Condition}.
 It {(chosenProduct.Sold ? "is not available." : $"has been in stock for {timeInStock.Days} days.")}");
+}
 
 
 //* Calculator example from lesson...
